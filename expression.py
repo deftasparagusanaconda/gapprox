62b4eb18779ea1b4f477d5da8a98ad64425fad91
deftasparagusanaconda <deftasparagusanaconda@users.noreply.github.com>
2025-03-21 22:15:28 +0530

    change repo structure

---
+++ expression.py
@@ -0,0 +1,20 @@
+# y = a*x + b
+def line(params, params_type = ,output_type = ["values"], force = False):
+	if 2 != len(params) and False == force:
+		return ValueError("line() got", len(params), "parameters")
+	elif True == force
+	
+	output = []
+	
+	if "values" in output_type:
+		for i in range():
+			
+	
+	if "string" in output_type:
+		output.append(str(params[0]) + "*x + " + str(params[1])
+	
+	return output
+
+# y = a*x**2 + b*x + c
+def parabola(params, output_type = ["values"], force = False):
+	if 
---
+++ main_cli.py
@@ -0,0 +1,5 @@
+# interactive CLI, meant to be more powerful than GUI
+
+import approximate, err_min_alg_iter, step, error, interpolate
+
+
