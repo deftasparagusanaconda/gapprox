8661350ce5284167a46a3aba8a9983bdf6e5805f
JeffrinSanton9 <jeffrinsanton0909@gmail.com>
2025-04-13 12:37:58 +0530

    Added plotters.py

---
+++ graphapproximator/components/plotters.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Here the function is the function to plot
+def plotter(function ,start_range , end_range,):
+    x = np.linspace(start_range , end_range , 400)
+    y = eval(function)
+    plt.plot(x , y)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('Plot of' + str(function))
+    
+    plt.grid(True)
+    plt.show()
+    
