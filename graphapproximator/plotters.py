046e19c021bdb84302a05a6b2e2cbeacb533be21
JeffrinSanton9 <jeffrinsanton0909@gmail.com>
2025-04-16 18:26:05 +0530

    Added ai.py

--- graphapproximator/plotters.py
+++ graphapproximator/plotters.py
@@ -1,13 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
+def remove_np(function):
+    
 #Here the function is the function to plot
-def plotter(function ,start_range , end_range,):
-    x = np.linspace(start_range , end_range , 400)
+def plotter(function ,start_range , end_range , resolution : int = 400):
+    x = np.linspace(start_range , end_range , resolution)
     y = eval(function)
     plt.plot(x , y)
     plt.xlabel('x')
     plt.ylabel('y')
+    function = remove_np(function)
     plt.title('Plot of' + str(function))
     
     plt.grid(True)
