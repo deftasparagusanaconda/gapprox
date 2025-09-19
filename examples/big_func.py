a8f9df028bba3e58b41de97624103db12cb6ed30
deftasparagusanaconda <deftasparagusanaconda@users.noreply.github.com>
2025-09-19 12:54:20 +0530

    checkpoint: pre-overhaul commit.
    next following commits will introduce breaking changes but will clean up the class namespace by an astronomical amount. see 'documentation/personal rants/dag.txt'

--- examples/big_func.py
+++ examples/big_func.py
@@ -1,6 +1,8 @@
 import gapprox as ga
 
-x, y, z = ga.make_variables('x', 'y', 'z')
+variables = ['x', 'y', 'z']
+
+x, y, z = (ga.Variable(v) for v in variables)
 expr = 'sin(x + y) * cos(z) + log(abs(x*y - z + 1)) + exp(sin(y) * cos(x)) + (x**2 + y**2 + z**2)**0.5 + tanh(x*y -z) + sqrt(abs(sin(x*z) + cos(y))) + (log(abs(x+1)) + exp(y*z) - sin(z)) / (1 + x**2 + y**2)'
 f = ga.Function(expr, x, y, z)
 f.dag.visualize()
